4d5fc2a6184de516f446c11f986edb9e85b23edd
Dyana Wu <dyanawu@gmail.com>
2020-08-18 17:57:14 +0800

    Complete through Part 4 Step 3

--- src/components/Fave.js
+++ src/components/Fave.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import AddToQueueIcon from '@material-ui/icons/AddToQueue';
 
 export default class Fave extends Component {
+  state = {
+    isFave: false
+  };
+
   handleClick = (e) => {
     console.log(e);
     console.log("click");
+    this.setState(prevState => ({ isFave: !prevState.isFave }));
     e.stopPropagation();
   }
 
   render() {
+    const isFave = this.state.isFave ? "remove_from_queue" : "add_to_queue";
+    let faveClass = `film-row-fave ${isFave}`;
     return (
       <div
         onClick={this.handleClick}
-        className="film-row-fave add_to_queue">
+        className={faveClass} >
         <AddToQueueIcon />
       </div>
     );
