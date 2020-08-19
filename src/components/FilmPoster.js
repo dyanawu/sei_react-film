da054b00e52a970c77fc538cd3434a2d7516bf11
Dyana Wu <dyanawu@gmail.com>
2020-08-19 17:11:26 +0800

    Complete Task 2 Step 1

--- src/components/FilmPoster.js
+++ src/components/FilmPoster.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class FilmPoster extends Component {
-  render() {
-    let film = this.props.film;
-    let posterUrl = `https://image.tmdb.org/t/p/w780/${film.poster_path}`;
+export default function FilmPoster(props) {
+  let film = props.film;
+  let posterUrl = `https://image.tmdb.org/t/p/w780/${film.poster_path}`;
 
-    return (
+  return (
       <img src={posterUrl} alt="" />
-    );
-  }
+  );
 }
