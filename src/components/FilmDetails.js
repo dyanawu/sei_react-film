2c8965d56cd9bbac221011c256fc15e5b235e15a
Dyana Wu <dyanawu@gmail.com>
2020-08-19 17:12:16 +0800

    Complete Task 2 Step 2

--- src/components/FilmDetails.js
+++ src/components/FilmDetails.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class FilmDetails extends Component {
-  render() {
-    return(
-      <div className="film-details">
-        <h1 className="section-title">DETAILS</h1>
-      </div>
-    );
-  }
+export default function FilmDetails(props) {
+  return(
+    <div className="film-details">
+      <h1 className="section-title">DETAILS</h1>
+    </div>
+  );
 }
