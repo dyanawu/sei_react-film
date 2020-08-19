b0119aa99aad6bece17c312005a5796e4647c6c5
Dyana Wu <dyanawu@gmail.com>
2020-08-19 17:18:15 +0800

    Complete Task 3 Step 1

--- src/components/FilmDetails.js
+++ src/components/FilmDetails.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export default function FilmDetails(props) {
+  const backdropUrl = `https://image.tmdb.org/t/p/w1280/${props.film.backdrop_path}`;
+  const posterUrl = `https://image.tmdb.org/t/p/w780/${props.film.poster_path}`;
+
   return(
     <div className="film-details">
       <h1 className="section-title">DETAILS</h1>
