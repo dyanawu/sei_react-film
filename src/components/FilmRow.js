8a58c0f1ecd94486901edea2290d6fcd7ecfe463
Dyana Wu <dyanawu@gmail.com>
2020-08-19 17:17:30 +0800

    Complete Task 2 Step 3

--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import FilmPoster from './FilmPoster';
 import Fave from './Fave';
 
-export default class FilmRow extends Component {
-  render() {
-    let film = this.props.film;
+export default function FilmRow(props) {
+  let film = props.film;
 
-    return (
-      <div
-        onClick = {this.props.onFilmClick}
-        className="film-row">
-        <FilmPoster film={film} />
-        <div className="film-summary">
-          <h1>{film.title}</h1>
-          <p>{new Date(film.release_date).getFullYear()}</p>
-        </div>
-        <Fave
-          isFave={this.props.isFave}
-          onFaveToggle={this.props.onFaveToggle}
-        />
+  return (
+    <div
+      onClick = {props.onFilmClick}
+      className="film-row"
+    >
+
+      <FilmPoster film={film} />
+
+      <div className="film-summary">
+        <h1>{film.title}</h1>
+        <p>{new Date(film.release_date).getFullYear()}</p>
       </div>
-    );
-  }
+
+      <Fave
+        isFave={props.isFave}
+        onFaveToggle={props.onFaveToggle}
+      />
+
+    </div>
+  );
 }
